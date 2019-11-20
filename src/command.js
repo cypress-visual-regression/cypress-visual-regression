60d2773c4f16d52aca10b036c5ebe7a364654184
Matúš Duchoň <matus.duchon@gmail.com>
2019-11-20 18:14:13 +0100

    :lipstick: Prettier

--- src/command.js
+++ src/command.js
@@ -1,32 +1,36 @@
 /* eslint-disable no-undef */
 
 function compareSnapshotCommand() {
-  Cypress.Commands.add('compareSnapshot', { prevSubject: 'optional' }, (subject, name, errorThreshold = 0.0) => {
-    let title = 'actual';
-    if (Cypress.env('type') === 'base') {
-      title = 'base';
-    }
+  Cypress.Commands.add(
+    'compareSnapshot',
+    { prevSubject: 'optional' },
+    (subject, name, errorThreshold = 0.0) => {
+      let title = 'actual';
+      if (Cypress.env('type') === 'base') {
+        title = 'base';
+      }
 
-    // take snapshot
-    if (subject) {
-      cy.get(subject).screenshot(`${name}-${title}`);
-    } else {
-      cy.screenshot(`${name}-${title}`);
-    }
+      // take snapshot
+      if (subject) {
+        cy.get(subject).screenshot(`${name}-${title}`);
+      } else {
+        cy.screenshot(`${name}-${title}`);
+      }
 
-    // run visual tests
-    if (Cypress.env('type') === 'actual') {
-      const options = {
-        fileName: name,
-        specDirectory: Cypress.spec.name,
-      };
-      cy.task('compareSnapshotsPlugin', options).then((results) => {
-        if (results.percentage > errorThreshold) {
-          throw new Error(`${name} images are different`);
-        }
-      });
+      // run visual tests
+      if (Cypress.env('type') === 'actual') {
+        const options = {
+          fileName: name,
+          specDirectory: Cypress.spec.name,
+        };
+        cy.task('compareSnapshotsPlugin', options).then((results) => {
+          if (results.percentage > errorThreshold) {
+            throw new Error(`${name} images are different`);
+          }
+        });
+      }
     }
-  });
+  );
 }
 
 /* eslint-enable no-undef */
