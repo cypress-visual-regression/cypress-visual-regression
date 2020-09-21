9eb950c6c5dc526301d833933d88f09b55afd76c
gabbersepp <gabbersepp@googlemail.com>
2020-09-21 21:31:54 +0200

    revert path settings, add extra test for environment path settings

--- docker/cypress/plugins/index.js
+++ docker/cypress/plugins/index.js
@@ -1,5 +1,12 @@
+const fs = require("fs");
 const getCompareSnapshotsPlugin = require('../../dist/plugin.js');
 
 module.exports = (on) => {
   getCompareSnapshotsPlugin(on);
+
+  on("task", {
+    doesExist: path => {
+      return fs.existsSync(path);
+    }
+  })
 };
