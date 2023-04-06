0fa5deda6a62a0c89a2b3b7a26f9ddde4d3567c5
Jason Hyde <github@2bad.me>
2023-04-06 15:36:29 +0200

    drop semicolons and trailing commas as per `standard` rules

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,67 +1,67 @@
-import * as fs from 'fs';
-import * as fsp from 'fs/promises';
-import * as path from 'path';
+import * as fs from 'fs'
+import * as fsp from 'fs/promises'
+import * as path from 'path'
 
-import { PNG } from 'pngjs';
-import pixelmatch from 'pixelmatch';
-import sanitize from 'sanitize-filename';
+import { PNG } from 'pngjs'
+import pixelmatch from 'pixelmatch'
+import sanitize from 'sanitize-filename'
 
-import { adjustCanvas, createFolder, parseImage, errorSerialize } from './utils';
-import { getValueOrDefault } from './utils-browser';
+import { adjustCanvas, createFolder, parseImage, errorSerialize } from './utils'
+import { getValueOrDefault } from './utils-browser'
 
-let CYPRESS_SCREENSHOT_DIR: string;
+let CYPRESS_SCREENSHOT_DIR: string
 
-interface MoveSnapshotArgs {
-  fromPath: string;
-  specDirectory: string;
-  fileName: string;
+type MoveSnapshotArgs = {
+  fromPath: string
+  specDirectory: string
+  fileName: string
 }
 
 /** Move the generated snapshot .png file to its new path.
  * The target path is constructed from parts at runtime in node to be OS independent.  */
 async function moveSnapshot(args: MoveSnapshotArgs) {
-  const { fromPath, specDirectory, fileName } = args;
-  const destDir = path.join(CYPRESS_SCREENSHOT_DIR, specDirectory);
-  const destFile = path.join(destDir, fileName);
+  const { fromPath, specDirectory, fileName } = args
+  const destDir = path.join(CYPRESS_SCREENSHOT_DIR, specDirectory)
+  const destFile = path.join(destDir, fileName)
 
-  return createFolder(destDir, false)
+  return await createFolder(destDir, false)
     .then(() => fsp.rename(fromPath, destFile))
-    .then(() => null);
+    .then(() => null)
 }
 
-interface UpdateSnapshotArgs {
-  name: string;
-  screenshotsFolder?: string;
-  snapshotBaseDirectory?: string;
-  specDirectory: string;
+type UpdateSnapshotArgs = {
+  name: string
+  screenshotsFolder?: string
+  snapshotBaseDirectory?: string
+  specDirectory: string
 }
 
 /** Update the base snapshot .png by copying the generated snapshot to the base snapshot directory.
  * The target path is constructed from parts at runtime in node to be OS independent.  */
 async function updateSnapshot(args: UpdateSnapshotArgs) {
-  const { name, screenshotsFolder, snapshotBaseDirectory, specDirectory } = args;
-  const toDir = getValueOrDefault(snapshotBaseDirectory, path.join(process.cwd(), 'cypress', 'snapshots', 'base'));
-  const snapshotActualDirectory = getValueOrDefault(screenshotsFolder, 'cypress/screenshots');
+  const { name, screenshotsFolder, snapshotBaseDirectory, specDirectory } = args
+  const toDir = getValueOrDefault(snapshotBaseDirectory, path.join(process.cwd(), 'cypress', 'snapshots', 'base'))
+  const snapshotActualDirectory = getValueOrDefault(screenshotsFolder, 'cypress/screenshots')
 
-  const destDir = path.join(toDir, specDirectory);
-  const fromPath = path.join(snapshotActualDirectory, specDirectory, `${name}.png`);
+  const destDir = path.join(toDir, specDirectory)
+  const fromPath = path.join(snapshotActualDirectory, specDirectory, `${name}.png`)
 
-  const destFile = path.join(destDir, `${name}.png`);
+  const destFile = path.join(destDir, `${name}.png`)
 
-  return createFolder(destDir, false)
+  return await createFolder(destDir, false)
     .then(() => fsp.copyFile(fromPath, destFile))
-    .then(() => null);
+    .then(() => null)
 }
 
-interface CompareSnapshotsPluginArgs {
-  failSilently?: boolean;
-  baseDir?: string;
-  diffDir?: string;
-  keepDiff?: boolean;
-  allowVisualRegressionToFail?: boolean;
-  fileName: string;
-  errorThreshold: number;
-  specDirectory: string;
+type CompareSnapshotsPluginArgs = {
+  failSilently?: boolean
+  baseDir?: string
+  diffDir?: string
+  keepDiff?: boolean
+  allowVisualRegressionToFail?: boolean
+  fileName: string
+  errorThreshold: number
+  specDirectory: string
 }
 
 /** Cypress plugin to compare image snapshots & generate a diff image.
@@ -72,35 +72,35 @@ async function compareSnapshotsPlugin(args: CompareSnapshotsPluginArgs) {
   const snapshotBaseDirectory = getValueOrDefault(
     args.baseDir,
     path.join(process.cwd(), 'cypress', 'snapshots', 'base')
-  );
+  )
   const snapshotDiffDirectory = getValueOrDefault(
     args.diffDir,
     path.join(process.cwd(), 'cypress', 'snapshots', 'diff')
-  );
-  const alwaysGenerateDiff = !(args.keepDiff === false);
-  const allowVisualRegressionToFail = args.allowVisualRegressionToFail === true;
+  )
+  const alwaysGenerateDiff = !(args.keepDiff === false)
+  const allowVisualRegressionToFail = args.allowVisualRegressionToFail === true
 
-  const fileName = sanitize(args.fileName);
+  const fileName = sanitize(args.fileName)
 
   const options = {
     actualImage: path.join(CYPRESS_SCREENSHOT_DIR, args.specDirectory, `${fileName}.png`),
     expectedImage: path.join(snapshotBaseDirectory, args.specDirectory, `${fileName}.png`),
-    diffImage: path.join(snapshotDiffDirectory, args.specDirectory, `${fileName}.png`),
-  };
+    diffImage: path.join(snapshotDiffDirectory, args.specDirectory, `${fileName}.png`)
+  }
 
-  let mismatchedPixels = 0;
-  let percentage = 0;
+  let mismatchedPixels = 0
+  let percentage = 0
   try {
-    await createFolder(snapshotDiffDirectory, args.failSilently);
-    const imgExpected = await parseImage(options.expectedImage);
-    const imgActual = await parseImage(options.actualImage);
+    await createFolder(snapshotDiffDirectory, args.failSilently)
+    const imgExpected = await parseImage(options.expectedImage)
+    const imgActual = await parseImage(options.actualImage)
     const diff = new PNG({
       width: Math.max(imgActual.width, imgExpected.width),
-      height: Math.max(imgActual.height, imgExpected.height),
-    });
+      height: Math.max(imgActual.height, imgExpected.height)
+    })
 
-    const imgActualFullCanvas = adjustCanvas(imgActual, diff.width, diff.height);
-    const imgExpectedFullCanvas = adjustCanvas(imgExpected, diff.width, diff.height);
+    const imgActualFullCanvas = adjustCanvas(imgActual, diff.width, diff.height)
+    const imgExpectedFullCanvas = adjustCanvas(imgExpected, diff.width, diff.height)
 
     mismatchedPixels = pixelmatch(
       imgActualFullCanvas.data,
@@ -109,49 +109,49 @@ async function compareSnapshotsPlugin(args: CompareSnapshotsPluginArgs) {
       diff.width,
       diff.height,
       { threshold: 0.1 }
-    );
-    percentage = (mismatchedPixels / diff.width / diff.height) ** 0.5;
+    )
+    percentage = (mismatchedPixels / diff.width / diff.height) ** 0.5
 
     if (percentage > args.errorThreshold) {
-      const specFolder = path.join(snapshotDiffDirectory, args.specDirectory);
-      await createFolder(specFolder, args.failSilently);
-      diff.pack().pipe(fs.createWriteStream(options.diffImage));
+      const specFolder = path.join(snapshotDiffDirectory, args.specDirectory)
+      await createFolder(specFolder, args.failSilently)
+      diff.pack().pipe(fs.createWriteStream(options.diffImage))
       if (!allowVisualRegressionToFail)
         throw new Error(
           `The "${fileName}" image is different. Threshold limit exceeded! \nExpected: ${args.errorThreshold} \nActual: ${percentage}`
-        );
+        )
     } else if (alwaysGenerateDiff) {
-      const specFolder = path.join(snapshotDiffDirectory, args.specDirectory);
-      await createFolder(specFolder, args.failSilently);
-      diff.pack().pipe(fs.createWriteStream(options.diffImage));
+      const specFolder = path.join(snapshotDiffDirectory, args.specDirectory)
+      await createFolder(specFolder, args.failSilently)
+      diff.pack().pipe(fs.createWriteStream(options.diffImage))
     }
   } catch (error) {
-    return { error: errorSerialize(error as Error) };
+    return { error: errorSerialize(error as Error) }
   }
   return {
     mismatchedPixels,
-    percentage,
-  };
+    percentage
+  }
 }
 
-interface PluginConfig extends Cypress.PluginConfig {
-  snapshotActualDirectory: string;
-}
+type PluginConfig = {
+  snapshotActualDirectory: string
+} & Cypress.PluginConfig
 
 /** Install plugin to compare snapshots.
  * (Also installs an internally used plugin to move snapshot files). */
 function getCompareSnapshotsPlugin(on: Cypress.PluginEvents, config: PluginConfig): void {
-  setupScreenshotPath(config);
+  setupScreenshotPath(config)
   on('task', {
     compareSnapshotsPlugin,
     moveSnapshot,
-    updateSnapshot,
-  });
+    updateSnapshot
+  })
 }
 
 function setupScreenshotPath(config: PluginConfig) {
   // use cypress default path as fallback
-  CYPRESS_SCREENSHOT_DIR = getValueOrDefault(config?.snapshotActualDirectory, 'cypress/screenshots');
+  CYPRESS_SCREENSHOT_DIR = getValueOrDefault(config?.snapshotActualDirectory, 'cypress/screenshots')
 }
 
-export default getCompareSnapshotsPlugin;
+export default getCompareSnapshotsPlugin
