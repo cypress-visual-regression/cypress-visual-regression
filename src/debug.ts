846c4248517bdef3c5264c0d4eb2e2da1fbadd62
Jason Hyde <github@2bad.me>
2023-04-07 02:30:10 +0200

    apply linting rules with autofix

--- src/debug.ts
+++ src/debug.ts
@@ -1,6 +1,6 @@
-import Debug from "debug";
+import Debug from 'debug'
 
 // Wrap debug module to ensure common namespace
 export default function (name: string): Debug.Debugger {
-  return Debug(`cypress-visual-regression: ${name}`);
+  return Debug(`cypress-visual-regression: ${name}`)
 }
--- src/utils.spec.ts
+++ src/utils.spec.ts
@@ -1,78 +1,72 @@
-import { PNG } from "pngjs";
-import {
-  adjustCanvas,
-  errorSerialize,
-  createFolder,
-  parseImage,
-} from "./utils";
-import { vi } from "vitest";
+import { PNG } from 'pngjs'
+import { adjustCanvas, errorSerialize, createFolder, parseImage } from './utils'
 
-vi.mock("fs/promises");
+vi.mock('fs/promises')
 
-describe("utils module", () => {
-  describe("adjustCanvas", () => {
-    it("should return the same image if given same width and height than the given image ", () => {
+describe('utils module', () => {
+  describe('adjustCanvas', () => {
+    it('should return the same image if given same width and height than the given image ', () => {
       const originalPNG = new PNG({
         width: 10,
-        height: 20,
-      });
-      const outputPNG = adjustCanvas(originalPNG, 10, 20);
-      expect(originalPNG).toEqual(outputPNG);
-    });
-    it("should return a new image based on the image given , with the new width and height passed", () => {
-      const originalPNG = new PNG({ width: 10, height: 20 });
-      const outputPNG = adjustCanvas(originalPNG, 50, 70);
-      expect(originalPNG).not.toEqual(outputPNG);
-      expect(outputPNG.width).toEqual(50);
-      expect(outputPNG.height).toEqual(70);
-    });
-  });
-  describe("createFolder", () => {
+        height: 20
+      })
+      const outputPNG = adjustCanvas(originalPNG, 10, 20)
+      expect(originalPNG).toEqual(outputPNG)
+    })
+    it('should return a new image based on the image given , with the new width and height passed', () => {
+      const originalPNG = new PNG({ width: 10, height: 20 })
+      const outputPNG = adjustCanvas(originalPNG, 50, 70)
+      expect(originalPNG).not.toEqual(outputPNG)
+      expect(outputPNG.width).toEqual(50)
+      expect(outputPNG.height).toEqual(70)
+    })
+  })
+  describe('createFolder', () => {
     afterEach(() => {
-      vi.restoreAllMocks();
-    });
+      vi.restoreAllMocks()
+    })
 
-    it("should return true if success", async () => {
-      const fs = await import("fs/promises");
-      fs.mkdir = vi.fn().mockReturnValue(undefined);
-      const result = await createFolder("url-path");
-      expect(fs.mkdir).toHaveBeenCalled();
-      expect(result).toEqual(true);
-    });
+    it('should return true if success', async () => {
+      const fs = await import('fs/promises')
+      fs.mkdir = vi.fn().mockReturnValue(undefined)
+      const result = await createFolder('url-path')
+      expect(fs.mkdir).toHaveBeenCalled()
+      expect(result).toEqual(true)
+    })
 
-    it("should throw if failing", async () => {
-      const fs = await import("fs/promises");
-      fs.mkdir = vi.fn().mockRejectedValue(new Error("mock error"));
-      const promise = createFolder("url-path");
-      expect(fs.mkdir).toHaveBeenCalled();
-      await expect(promise).rejects.toEqual(new Error("mock error"));
-    });
+    it('should throw if failing', async () => {
+      const fs = await import('fs/promises')
+      fs.mkdir = vi.fn().mockRejectedValue(new Error('mock error'))
+      const promise = createFolder('url-path')
+      expect(fs.mkdir).toHaveBeenCalled()
+      await expect(promise).rejects.toEqual(new Error('mock error'))
+    })
 
-    it("should return false if failing and failSilently is set to true", async () => {
-      const fs = await import("fs/promises");
-      fs.mkdir = vi.fn().mockRejectedValue(new Error("mock error"));
-      const result = await createFolder("url-path", true);
-      expect(fs.mkdir).toHaveBeenCalled();
-      expect(result).toEqual(false);
-    });
-  });
-  describe("errorSerialize", () => {
-    it("should serialize an object into a string", () => {
-      const errorObject = { error: "I'm a teapot", code: 418 };
-      const output = errorSerialize(errorObject);
+    it('should return false if failing and failSilently is set to true', async () => {
+      const fs = await import('fs/promises')
+      fs.mkdir = vi.fn().mockRejectedValue(new Error('mock error'))
+      const result = await createFolder('url-path', true)
+      expect(fs.mkdir).toHaveBeenCalled()
+      expect(result).toEqual(false)
+    })
+  })
+  describe('errorSerialize', () => {
+    it('should serialize an object into a string', () => {
+      const errorObject = { error: "I'm a teapot", code: 418 }
+      const output = errorSerialize(errorObject)
 
-      expect(output).toBe('{"error":"I\'m a teapot","code":418}');
-    });
-  });
-  describe("parseImage", () => {
+      expect(output).toBe('{"error":"I\'m a teapot","code":418}')
+    })
+  })
+  describe('parseImage', () => {
     // TODO mock existsSync
-    it("should throw error when image does not exist", async () => {
-      const promise = parseImage("img");
-      await expect(promise).rejects.toThrow("Snapshot img does not exist.");
-    });
+    it('should throw error when image does not exist', async () => {
+      const promise = parseImage('img')
+      await expect(promise).rejects.toThrow('Snapshot img does not exist.')
+    })
     // TODO mock createReadStream
-    it("should return an error on PNG creation", async () => {});
+    it('should return an error on PNG creation', async () => {})
     // TODO mock createReadStream
-    it("should return a PNG image from reference", async () => {});
-  });
-});
+    it('should return a PNG image from reference', async () => {})
+  })
+})
