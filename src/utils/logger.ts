aef84eb859ad7f9a27d05ba101de44442602ceb2
rerades <rerades@siete3.com>
2023-10-21 22:02:49 +0200

    improving logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,18 +2,12 @@ import { createLogger, format, transports } from 'winston'
 
 const logger = createLogger({
   level: 'info',
-  format: format.json(),
-  defaultMeta: { service: 'user-service' },
+  format: format.combine(format.splat(), format.json()),
   transports: [
-    //
-    // - Write all logs with importance level of `error` or less to `error.log`
-    // - Write all logs with importance level of `info` or less to `combined.log`
-    //
     new transports.File({ filename: 'error.log', level: 'error' }),
     new transports.File({ filename: 'combined.log' })
   ]
 })
-
 //
 // If we're not in production then log to the `console` with the format:
 // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
@@ -21,7 +15,20 @@ const logger = createLogger({
 if (process.env.NODE_ENV !== 'production') {
   logger.add(
     new transports.Console({
-      format: format.combine(format.colorize(), format.simple())
+      format: format.combine(
+        format.timestamp({
+          format: 'YY-MM-DD HH:mm:ss'
+        }),
+        format.label({
+          label: '[LOGGER]'
+        }),
+        format.simple(),
+        format.printf((msg) =>
+          format
+            .colorize()
+            .colorize(msg.level, `${msg.timestamp as string} - ${msg.label as string}: ${msg.message as string}`)
+        )
+      )
     })
   )
 }
