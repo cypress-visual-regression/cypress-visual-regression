ac8bc9188b6f6ce6b86fc868db32470139772625
rerades <rerades@siete3.com>
2023-10-25 21:05:56 +0200

    adding test for updateSnapshots

--- src/plugin.spec.ts
+++ src/plugin.spec.ts
@@ -1,37 +1,56 @@
 import { PNG } from 'pngjs'
-import { generateImage } from './plugin'
+import { generateImage, updateSnapshot } from './plugin'
 import { expect } from 'vitest'
 import { unlinkSync } from 'node:fs'
 import path from 'node:path'
 
-// unit tests for generateImage
-describe('generateImage', () => {
-  const validImagePath = 'mocks/test.png'
-  describe('when the image is generated', () => {
-    afterEach(() => {
-      unlinkSync(path.join(validImagePath))
+describe('plugin', () => {
+  // unit tests for generateImage
+  describe('generateImage', () => {
+    const validImagePath = 'mocks/test.png'
+    describe('when the image is generated', () => {
+      it('should generate an image', async () => {
+        const png = new PNG({ width: 1, height: 1 })
+        png.data = Buffer.from([255, 0, 0, 255])
+        const result = await generateImage(png, validImagePath)
+        expect(result).toBe(true)
+        unlinkSync(path.join(validImagePath))
+      })
     })
-    it('should generate an image', async () => {
-      const png = new PNG({ width: 1, height: 1 })
-      png.data = Buffer.from([255, 0, 0, 255])
-      const result = await generateImage(png, validImagePath)
-      expect(result).toBe(true)
+    describe('when the image is not generated', () => {
+      const systemFileName = '/System/wadus/test.png'
+      const rootFileName = '/test.png'
+      it('should not generate an image and throw an error on directory creation', async () => {
+        const png = new PNG({ width: 1, height: 1 })
+        png.data = Buffer.from([255, 0, 0, 255])
+        const result = generateImage(png, systemFileName)
+        await expect(result).rejects.toThrow(`cannot create directory '${path.dirname(systemFileName)}'.`)
+      })
+      it('should not generate an image and throw an error on file creation', async () => {
+        const png = new PNG({ width: 1, height: 1 })
+        png.data = Buffer.from([255, 0, 0, 255])
+        const result = generateImage(png, rootFileName)
+        await expect(result).rejects.toThrow(`cannot create file '${rootFileName}'.`)
+      })
     })
   })
-  describe('when the image is not generated', () => {
-    const systemFileName = '/System/wadus/test.png'
-    const rootFileName = '/test.png'
-    it('should not generate an image and throw an error on directory creation', async () => {
-      const png = new PNG({ width: 1, height: 1 })
-      png.data = Buffer.from([255, 0, 0, 255])
-      const result = generateImage(png, systemFileName)
-      await expect(result).rejects.toThrow(`cannot create directory '${path.dirname(systemFileName)}'.`)
-    })
-    it('should not generate an image and throw an error on file creation', async () => {
-      const png = new PNG({ width: 1, height: 1 })
-      png.data = Buffer.from([255, 0, 0, 255])
-      const result = generateImage(png, rootFileName)
-      await expect(result).rejects.toThrow(`cannot create file '${rootFileName}'.`)
+  describe('updateSnapshot', () => {
+    const screenshotName = 'new-image'
+    const specName = 'assets'
+    const screenshotAbsolutePath = path.join('fixtures', 'assets', 'enjuto.png')
+    const defaultBaseDirectory = path.join('cypress', 'snapshots', 'base')
+    const baseDirectory = path.join('output')
+    describe('when the snapshot is updated', () => {
+      it('should copy the snapshot to default folder', async () => {
+        const result = await updateSnapshot({ screenshotName, specName, screenshotAbsolutePath })
+        expect(result).toBe(true)
+        unlinkSync(path.join(defaultBaseDirectory, specName, `${screenshotName}.png`))
+      })
+      it('should copy the snapshot to baseDirectory', async () => {
+        const result = await updateSnapshot({ baseDirectory, screenshotName, specName, screenshotAbsolutePath })
+        expect(result).toBe(true)
+        unlinkSync(path.join(baseDirectory, specName, `${screenshotName}.png`))
+      })
     })
   })
 })
