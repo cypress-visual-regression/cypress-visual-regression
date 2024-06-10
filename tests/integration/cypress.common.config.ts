440bdcf29774eab1c0cb59bba255858d368758dc
Jovan Ilić <ilicjovan89@gmail.com>
2024-06-10 23:38:18 +0200

    Refactor cypress tests

---
+++ tests/integration/cypress.common.config.ts
@@ -0,0 +1,45 @@
+import * as fs from 'fs'
+import { configureVisualRegression } from '@src/plugin'
+import webpackPreprocessor from '@cypress/webpack-preprocessor'
+import webpackConfig from './webpack.config'
+
+const cypressCommonConfig: Cypress.ConfigOptions = {
+  trashAssetsBeforeRuns: true,
+  viewportHeight: 720,
+  viewportWidth: 1280,
+  video: false,
+  e2e: {
+    screenshotsFolder: './cypress/snapshots/actual',
+    setupNodeEvents(on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions) {
+      on('before:browser:launch', (browser, launchOptions) => {
+        if (browser.name === 'chrome' && browser.family === 'chromium') {
+          launchOptions.args.push('--force-device-scale-factor=1')
+        }
+        return launchOptions
+      })
+      configureVisualRegression(on)
+      on('task', {
+        doesExist: (path: string) => fs.existsSync(path)
+      })
+      on('task', {
+        log(args) {
+          console.log(...args)
+          return null
+        }
+      })
+
+      // required for enabling typescript aliases
+      on('file:preprocessor', webpackPreprocessor({ webpackOptions: webpackConfig }))
+
+      return config
+    }
+  }
+}
+
+export const cypressConfigWithEnv = (env: Cypress.ConfigOptions['env']): Cypress.ConfigOptions => ({
+  ...cypressCommonConfig,
+  env: {
+    ...cypressCommonConfig.env,
+    ...env
+  }
+})
---
+++ tests/integration/cypress/e2e/main.env.cy.ts
@@ -0,0 +1,57 @@
+import { CypressConfigEnv } from '@src/command'
+
+const visualRegressionConfig = Cypress.env()
+
+visualRegressionConfig.baseDirectory = './cypress/snapshots/alternative-base'
+visualRegressionConfig.diffDirectory = './cypress/snapshots/alternative-diff'
+visualRegressionConfig.generateDiff = 'always'
+
+const env: Partial<CypressConfigEnv> = {
+  ...Cypress.env(),
+  visualRegressionBaseDirectory: './cypress/snapshots/alternative-base',
+  visualRegressionDiffDirectory: './cypress/snapshots/alternative-diff',
+  visualRegressionGenerateDiff: 'always'
+}
+
+describe(
+  'Visual Regression Example with setting paths by environment variables',
+  {
+    env
+  },
+  () => {
+    it('take screenshot with parent command', () => {
+      if (env.visualRegressionType === 'base') {
+        cy.visit('./cypress/web/01.html')
+        cy.get('H1').contains('Hello, World')
+        cy.compareSnapshot('home')
+        cy.task('doesExist', `${env.visualRegressionBaseDirectory}/cypress/e2e/main.env.cy.ts/home.png`).should(
+          'be.true'
+        )
+      } else {
+        cy.visit('./cypress/web/01.html')
+        cy.get('H1').contains('Hello, World')
+        cy.compareSnapshot('home')
+        cy.task('doesExist', `${env.visualRegressionDiffDirectory}/cypress/e2e/main.env.cy.ts/home.png`).should(
+          'be.true'
+        )
+      }
+    })
+    it('take screenshot with child command', () => {
+      if (env.visualRegressionType === 'base') {
+        cy.visit('./cypress/web/01.html')
+        cy.get('H1').contains('Hello, World').compareSnapshot('home-child')
+        cy.task(
+          'doesExist',
+          `${visualRegressionConfig.baseDirectory}/cypress/e2e/main.env.cy.ts/home_child.png`
+        ).should('be.true')
+      } else {
+        cy.visit('./cypress/web/01.html')
+        cy.get('H1').contains('Hello, World').compareSnapshot('home-child')
+        cy.task(
+          'doesExist',
+          `${visualRegressionConfig.diffDirectory}/cypress/e2e/main.env.cy.ts/home_child.png`
+        ).should('be.true')
+      }
+    })
+  }
+)
