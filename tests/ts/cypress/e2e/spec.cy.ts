56aa6c34f8941a951285e7c66ec4e63acd0ae526
Davi Medeiros <davilima6@gmail.com>
2026-05-28 07:47:19 +0200

    test: cover compareSnapshot TypeScript usage

--- tests/ts/cypress/e2e/spec.cy.ts
+++ tests/ts/cypress/e2e/spec.cy.ts
@@ -2,5 +2,6 @@ describe('run cypress-visual-regression in ts project', () => {
   it('should run without errors', () => {
     cy.visit('https://example.cypress.io')
     cy.compareSnapshot('ts')
+    cy.get('h1').compareSnapshot('ts-heading', 0.2)
   })
 })
