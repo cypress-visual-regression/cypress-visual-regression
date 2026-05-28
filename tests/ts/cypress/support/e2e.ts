7a91b0d05c28057245233798147a6a91707c2bd1
Davi Medeiros <davilima6@gmail.com>
2026-05-28 07:47:19 +0200

    test: cover compareSnapshot TypeScript usage

--- tests/ts/cypress/support/e2e.ts
+++ tests/ts/cypress/support/e2e.ts
@@ -1,2 +1,19 @@
 import { addCompareSnapshotCommand } from 'cypress-visual-regression/dist/command'
 addCompareSnapshotCommand()
+
+Cypress.Commands.overwrite('compareSnapshot', (originalFn, ...args) => {
+  return originalFn(...args)
+})
+
+Cypress.Commands.overwrite('compareSnapshot', (originalFn, subject, name, commandOptions) => {
+  if (typeof commandOptions === 'number') {
+    return originalFn(subject, name, commandOptions)
+  }
+
+  return originalFn(subject, name, {
+    ...commandOptions,
+    onBeforeScreenshot($el: JQuery) {
+      commandOptions?.onBeforeScreenshot?.($el)
+    }
+  })
+})
